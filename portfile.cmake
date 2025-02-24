510e7910eadcdaf0a9a525da2649387ad941b96f
kingmaj0r <49806509+kingmaj0r@users.noreply.github.com>
2025-02-24 13:32:08 +0700

    Update portfile.cmake

--- portfile.cmake
+++ portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO filesverse/filerix
-  REF null
+  REF v1.0.3
   SHA512 1d29c53bb5c8392e973485095eb2bc475aec5db0f9a12bad3b7b965223fdaf66ce90f4b1b54273f47799601fbb84834df74ac282f723396b75865a45d3162834
 )
 
